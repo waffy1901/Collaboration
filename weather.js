03a7b5c05c76552cf0c27bc57306f85d8b4cfd28
waffy1901 <waffyahmed@gmail.com>
2023-06-29 13:40:30 -0400

    Added Promise functionality and temperature text to html

--- weather.js
+++ weather.js
@@ -1,111 +1,104 @@
 //location function --> get coordinates 
+// Location function: Get coordinates
 function getCoordinates() {
-    var options = {
+    return new Promise((resolve, reject) => {
+      var options = {
         enableHighAccuracy: true,
         timeout: 5000,
         maximumAge: 0
-    };
+      };
   
-    function success(pos) {
+      function success(pos) {
         var crd = pos.coords;
         var lat = crd.latitude.toString();
         var lng = crd.longitude.toString();
         var coordinates = [lat, lng];
         console.log(`Latitude: ${lat}, Longitude: ${lng}`);
-        getCity(coordinates);
-        console.log("City" + getCity(coordinates));
-        return;
+        resolve(coordinates); // Resolve the promise with the coordinates
+      }
   
-    }
-  
-    function error(err) {
+      function error(err) {
         console.warn(`ERROR(${err.code}): ${err.message}`);
-    }
+        reject(err); // Reject the promise with the error
+      }
   
-    navigator.geolocation.getCurrentPosition(success, error, options);
-}
-
-// get city
-function getCity(coordinates) {
-    var xhr = new XMLHttpRequest();
-    var lat = coordinates[0];
-    var lng = coordinates[1];
-    // var city_request = "";
-    // Paste your LocationIQ token below.
-    xhr.open('GET', "https://us1.locationiq.com/v1/reverse.php?key=pk.84900f437a1afaa4e93f8961f05cce39&lat=" +
-    lat + "&lon=" + lng + "&format=json", true);
-    xhr.send();
-    xhr.onreadystatechange = processRequest;
-    xhr.addEventListener("readystatechange", processRequest, false);
+      navigator.geolocation.getCurrentPosition(success, error, options);
+    });
+  }
   
-    function processRequest(e) {
+  // Get city
+  function getCity(coordinates) {
+    return new Promise((resolve, reject) => {
+      var xhr = new XMLHttpRequest();
+      var lat = coordinates[0];
+      var lng = coordinates[1];
+  
+      xhr.open(
+        'GET',
+        `https://us1.locationiq.com/v1/reverse.php?key=pk.84900f437a1afaa4e93f8961f05cce39&lat=${lat}&lon=${lng}&format=json`,
+        true
+      );
+      xhr.send();
+  
+      xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            var response = JSON.parse(xhr.responseText);
-            var city = response.address.city;
-            // city_request = city.toString();
-            console.log(city);
-            return city.toString();
+          var response = JSON.parse(xhr.responseText);
+          var city = response.address.city;
+          console.log(city);
+          resolve(city); // Resolve the promise with the city
         }
-    }
-    // return processRequest(city);
-}
-
-// console.log(getCoordinates());
-
-// get temperature
-    // high/low and current
-    // hourly and 7-day forecast
-function getTemperature() {
-    // Enter your API key here
+      };
+    });
+  }
+  
+  // Get temperature
+  function getTemperature() {
     const api_key = '4fc8b020e0e454b126b87d4078185ae5';
-
-    // base_url variable to store url
-    const base_url = 'http://api.openweathermap.org/data/2.5/weather?';
-
-    // Give city name
-    const coords = getCoordinates();
-    console.log("Coords: " + coords);
-    const city_name = getCity(coords);
-
-    // complete_url variable to store complete url address
-    const complete_url = `${base_url}appid=${api_key}&q=${city_name}`;
-
-    // Make a GET request to the API using fetch
-    fetch(complete_url)
-    .then(function (response) {
+    const base_url = 'https://api.openweathermap.org/data/2.5/weather?';
+  
+    getCoordinates()
+      .then((coordinates) => getCity(coordinates))
+      .then((city_name) => {
+        const complete_url = `${base_url}appid=${api_key}&q=${city_name}`;
+        return fetch(complete_url);
+      })
+      .then((response) => {
         if (response.ok) {
-        return response.json();
+          return response.json();
         } else {
-        throw new Error('City Not Found');
+          throw new Error('City Not Found');
         }
-    })
-    .then(function (data) {
-        const y = data.main;
-        const current_temperature = y.temp;
-        const current_pressure = y.pressure;
+      })
+      .then((data) => {
+        var y = data.main;
+        const current_temperature = Math.round((y.temp - 273.15) * 9/5 + 32);
+        const current_pressure = (y.pressure * 0.02952998057228486).toFixed(2);
         const current_humidity = y.humidity;
         const z = data.weather;
         const weather_description = z[0].description;
-
-        // Print the weather information
-        console.log(
-        ' Temperature (in kelvin unit) = ' +
-            current_temperature +
-            '\n atmospheric pressure (in hPa unit) = ' +
-            current_pressure +
-            '\n humidity (in percentage) = ' +
-            current_humidity +
-            '\n description = ' +
-            weather_description
-        );
-    })
-    .catch(function (error) {
+  
+        // console.log(
+        //   ' Temperature (in kelvin unit) = ' +
+        //     current_temperature +
+        //     '\n atmospheric pressure (in hPa unit) = ' +
+        //     current_pressure +
+        //     '\n humidity (in percentage) = ' +
+        //     current_humidity +
+        //     '\n description = ' +
+        //     weather_description
+        // );
+        document.getElementById("Temperature").innerHTML = 'Temperature: ' + current_temperature + '&deg; F';
+        document.getElementById("Pressure").innerHTML = 'Pressure: ' + current_pressure + ' Hg';
+        document.getElementById("Humidity").innerHTML = 'Humidity: ' + current_humidity + '%';
+        document.getElementById("weatherDescription").innerHTML = 'Current Weather Description: ' + weather_description;
+      })
+      .catch((error) => {
         console.log('Error:', error.message);
-    });
-}
-
-console.log(getTemperature());
-    
+      });
+  }
+  
+  getTemperature();
+  
 // converter function (F --> C)
 // get weather condition
 // add city
